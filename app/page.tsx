671f10478db86bba1f73a7a754776e2ae8a1509a
babono <babono@hotmail.com>
2023-11-26 19:02:26 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h2>Wew</h2>
+        <h2 className="">This is going to be a matchmaking App...</h2>
       </div>
     </main>
   );
